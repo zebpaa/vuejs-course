763ac8163e09f069c287e2efb33388431a5b065c
zebpaa <skvorcov_90@mail.ru>
2023-05-14 15:57:09 +0400

    added 7 lesson

---
+++ 007/src/Pattern.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container pt-1">
+    <div class="card">
+      <h2></h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components:{}
+}
+</script>
+
+<style scoped>
+
+</style>
