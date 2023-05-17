691260a7f274a820619ad7b4ba5fbb4b15abb9c2
zebpaa <skvorcov_90@mail.ru>
2023-05-18 00:58:41 +0400

    finished lesson 9

---
+++ 009/src/components/AppModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')"></div>
+  <div class="modal">
+    <h3>Это заголоовок модалки</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe ipsum autem corporis quae nam iusto in quo tempore cupiditate dolorem?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close']
+}
+</script>
+
+<style scoped>
+
+</style>
--- 009/src/main.js
+++ 009/src/main.js
@@ -1,9 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
+import translatePlugin from './translatePlugin'
 
 const app = createApp(App)
 
-app.use()
+const ru = {
+  app: {
+    title: 'Как работают плагины во Vue?',
+    changeBtn: 'Переключить язык'
+  }
+}
+
+const en = {
+  app: {
+    title: 'How plugins work in Vue?',
+    changeBtn: 'Toggle Language'
+  }
+}
+
+app.use(translatePlugin, {ru, en})
 
 app.mount('#app')
