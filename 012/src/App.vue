65f584973a9a45535a2f36cd74c15b293a21e9aa
zebpaa <skvorcov_90@mail.ru>
2023-05-22 21:19:40 +0400

    12 lesson in progress

---
+++ 012/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>Vue Composition Api</h1>
+      <small>data, methods, computed, watch</small>
+      <div class="form-control">
+        <input type="text" v-model="firstName">
+      </div>
+
+      <button class="btn" @click="change">Изменить</button>
+    </div>
+    <FrameworkInfo :name="name" :version="version"/>
+  </div>
+</template>
+
+<script>
+import {
+  ref, 
+  reactive, 
+  toRefs, 
+  isRef, 
+  isReactive, 
+  computed,
+  watch
+} from 'vue'
+import FrameworkInfo from './FrameworkInfo.vue'
+
+export default {
+  setup(){
+    const name = ref('VueJS')
+    const version = ref(3)
+    const textInput = ref(null)
+    const firstName = ref('')
+
+
+    const framework = reactive ({
+      name: 'VueJS',
+      version: 3
+    })
+
+    // const doubleVersion = computed(() => version.value * 2)
+
+    // watch([doubleVersion, name], (newValues, oldValues) => {
+    //   console.log('new version', newValues[0])
+    //   console.log('new name', newValues[1])
+    //   console.log('old version', oldValues[0])
+    //   console.log('old name', oldValues[1])
+    // })
+
+    watch(firstName, (newV, oldV) => {
+      console.log(newV)
+    })
+
+    function changeInfo(){
+      name.value = 'Vue JS !'
+      version.value = 42
+    }
+
+    return {
+      name, version,
+      change: changeInfo,
+      firstName
+    }
+  },
+  components: {FrameworkInfo}
+}
+</script>
---
+++ 012/src/FrameworkInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <p>Название: <strong>{{ name }}</strong></p>
+    <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
+
+    <button class="btn warning" @click="changeToThree">Изменить на 3 версию</button>
+  </div>
+</template>
+
+<script>
+import {computed} from 'vue'
+export default {
+  props: ['name', 'version'],
+  setup(props) {
+
+    function change(){
+      this.$emit()
+    }
+
+    return {
+      changeToThree: change,
+      doubleVersion: computed(() => props.version * 2)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
