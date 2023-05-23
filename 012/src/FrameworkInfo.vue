648ca1b9b8965ebc81c4d55625b4bbdbb9dad7ac
zebpaa <skvorcov_90@mail.ru>
2023-05-23 19:38:31 +0400

    finished 12 lvl

--- 012/src/FrameworkInfo.vue
+++ 012/src/FrameworkInfo.vue
@@ -3,23 +3,29 @@
     <p>Название: <strong>{{ name }}</strong></p>
     <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
 
+
     <button class="btn warning" @click="changeToThree">Изменить на 3 версию</button>
   </div>
 </template>
 
 <script>
-import {computed} from 'vue'
-export default {
-  props: ['name', 'version'],
-  setup(props) {
+import {computed, inject} from 'vue'
 
-    function change(){
-      this.$emit()
+export default {
+  emits: ['change-version'],
+  setup(props, context) {
+    function change() {
+      // logic
+      context.emit('change-version', 3)
     }
 
+    const version = inject('version')
+
     return {
+      name: inject('name'),
+      version: version,
       changeToThree: change,
-      doubleVersion: computed(() => props.version * 2)
+      doubleVersion: computed(() => version.value * 2)
     }
   }
 }
--- 012/src/main.js
+++ 012/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {router} from './router'
+import {store} from './store'
 import './theme.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount('#app')
